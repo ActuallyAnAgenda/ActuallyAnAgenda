e93ffd8e4d326030fea44d91182b5a2182c010b2
yeahbennou <38169671+yeahbennou@users.noreply.github.com>
2020-05-03 14:01:57 -0400

    a

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,21 @@
-//import com.google.auth.oauth2.GoogleCredentials;
-//import com.google.cloud.firestore.Firestore;
-//import com.google.firebase.FirebaseApp;
-//import com.google.firebase.FirebaseOptions;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.Firestore;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
 import java.util.*;
 
 public class Main
 {
+	Firestore db;
 
-	public static void main(String[] args)
+	public static void main(String[] args)throws Exception
 	{
-
-		User user = new User();
-
-		// Whenever there is a new task:
-		user.getSchedule().addTask(
-				new Task(
-						new Date(System.currentTimeMillis() + 60 * 1000 * 60 * 24 * 1), // should be the unix time
-						2.0,
-						(byte) 5,
-						"ISP"
-				)
-		);
-		for(ScheduleEvent se: user.getSchedule().getCurrentEventlist())
+		GetInfoFromDatabase gifb = new GetInfoFromDatabase();
+		ArrayList<Task> test = gifb.queryAllTasks("wcGsw8HnzDZNSbS0SEKOyBSGUIQ2");
+		for(Task t: test)
 		{
-			System.out.println(se.getBegin());
-			System.out.println(se.getEnd());
+			System.out.println(t);
 		}
+		System.out.println("done");
 	}
 }
